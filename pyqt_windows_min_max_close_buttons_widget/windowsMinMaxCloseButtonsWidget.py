f4c0c4180008a708d3a84e580a7fd122652881a7
Jung Gyu Yoon <yjg30737@gmail.com>
2022-02-17 11:15:09 +0900

    Fix typo

--- pyqt_windows_min_max_close_buttons_widget/windowsMinMaxCloseButtonsWidget.py
+++ pyqt_windows_min_max_close_buttons_widget/windowsMinMaxCloseButtonsWidget.py
@@ -1,7 +1,7 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPalette, QColor
 from PyQt5.QtWidgets import qApp, QMenuBar
-from pyqt_min_max_close_buttons import MinMaxCloseButtonsWidget
+from pyqt_min_max_close_buttons_widget import MinMaxCloseButtonsWidget
 
 from python_color_getter.pythonColorGetter import PythonColorGetter
 
