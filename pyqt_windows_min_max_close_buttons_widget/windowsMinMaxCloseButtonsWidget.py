5bc505bcf192ca7c191282260e9bef3b16e8b337
Jung Gyu Yoon <yjg30737@gmail.com>
2022-04-12 08:26:45 +0900

    Refactoring

--- pyqt_windows_min_max_close_buttons_widget/windowsMinMaxCloseButtonsWidget.py
+++ pyqt_windows_min_max_close_buttons_widget/windowsMinMaxCloseButtonsWidget.py
@@ -9,16 +9,24 @@ from python_color_getter.pythonColorGetter import PythonColorGetter
 class WindowsMinMaxCloseButtonsWidget(MinMaxCloseButtonsWidget):
     def __init__(self, base_widget: QWidget, hint=Qt.WindowMinMaxButtonsHint | Qt.WindowCloseButtonHint, font=QFont('Arial', 12)):
         super().__init__(hint)
-        self.__baseWidget = base_widget
-        self.__initUi(hint, font)
+        self.__initVal(base_widget, hint, font)
+        self.__initUi()
 
-    def __initUi(self, hint, font):
+    def __initVal(self, base_widget, hint, font):
+        self.__baseWidget = base_widget
+        self.__hint = hint
+        self.__font = font
+
+    def __initUi(self):
         self.layout().setSpacing(0)
 
         self._minimizeBtn.setText('🗕')
         self._maximizeBtn.setText('🗖')
         self._closeBtn.setText('🗙')
 
+        self.__styleInit()
+
+    def __styleInit(self):
         btns = [self._minimizeBtn, self._maximizeBtn, self._closeBtn]
 
         base_color = self.__baseWidget.palette().color(QPalette.Base)
@@ -33,8 +41,8 @@ class WindowsMinMaxCloseButtonsWidget(MinMaxCloseButtonsWidget):
                                                                         hover_color.blue())
         btn_text_color = QColor(lbl_r, lbl_g, lbl_b)
 
-        h_padding_size = font.pointSize() // 2
-        v_padding_size = font.pointSize() // 5
+        h_padding_size = self.__font.pointSize() // 2
+        v_padding_size = self.__font.pointSize() // 5
 
         tool_button_style = f'''
                             QPushButton
@@ -71,7 +79,7 @@ class WindowsMinMaxCloseButtonsWidget(MinMaxCloseButtonsWidget):
                              }}
                              '''
 
-        font_size = font.pointSize() // 1.2
+        font_size = self.__font.pointSize() // 1.2
 
         for btn in btns:
             font = btn.font()
@@ -80,3 +88,8 @@ class WindowsMinMaxCloseButtonsWidget(MinMaxCloseButtonsWidget):
             btn.setStyleSheet(tool_button_style)
 
         self._closeBtn.setStyleSheet(close_button_style)
+
+    def event(self, e):
+        if e.type() == 100:
+            self.__styleInit()
+        return super().event(e)
