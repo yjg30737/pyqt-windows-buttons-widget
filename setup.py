faf9724597312fcc2881d7703a2ce117fb2e45b3
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-11 18:08:27 +0900

    Change required packages url

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 setup(
     name='pyqt-windows-min-max-close-buttons-widget',
-    version='0.0.2',
+    version='0.0.1',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
@@ -11,7 +18,7 @@ setup(
     url='https://github.com/yjg30737/pyqt-windows-min-max-close-buttons-widget.git',
     install_requires=[
         'PyQt5>=5.8',
-        'python-color-getter @ git+https://git@github.com/yjg30737/python-color-getter.git@main',
-        'pyqt-min-max-close-buttons-widget @ git+https://git@github.com/yjg30737/pyqt-min-max-close-buttons-widget.git@main'
+        'python-color-getter>=0.0.1',
+        'pyqt-titlebar-buttons-widget @ git+https://git@github.com/yjg30737/pyqt-titlebar-buttons-widget.git@main'
     ]
 )
