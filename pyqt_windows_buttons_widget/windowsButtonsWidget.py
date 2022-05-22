1f4d01edc6e2648ce5dc44093498be1b171df2f6
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-22 14:12:57 +0900

    Always set button's background after StyleChange event emitting

--- pyqt_windows_buttons_widget/windowsButtonsWidget.py
+++ pyqt_windows_buttons_widget/windowsButtonsWidget.py
@@ -13,17 +13,17 @@ class WindowsButtonsWidget(TitlebarButtonsWidget):
         self._maximizeBtn.setText('🗖')
         self._closeBtn.setText('🗙')
 
-        # fill the button's background with color
-        background_color = self._base_widget.palette().color(QPalette.Base).name()
-        for btn in self._btn_dict.values():
-            btn.setBackground(background_color)
-
         self.__styleInit()
 
         self.__window = self._base_widget.window()
         self.__window.installEventFilter(self)
 
     def __styleInit(self):
+        # fill the button's background with color
+        background_color = self._base_widget.palette().color(QPalette.Base).name()
+        for btn in self._btn_dict.values():
+            btn.setBackground(background_color)
+
         close_button_style = self._closeBtn.styleSheet() + f'''
                              QPushButton:hover 
                              {{ 
